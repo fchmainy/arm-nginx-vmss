1bcca566b1e473e4de80aa94dd81413b0cf04aa5
Fouad Chmainy <fchmainy@yahoo.com>
2021-01-26 17:33:36 +0100

    changes in scripts

--- terraform/scripts/install-and-onboard.sh
+++ terraform/scripts/install-and-onboard.sh
@@ -11,7 +11,7 @@
 # $8. Controller API Key
 # $9. Environment service.
 
-CONTROLLER_URL=https://$1:8443/1.4
+#CONTROLLER_URL=https://$1:8443/1.4
 mkdir -p /etc/ssl/nginx
 
 echo $2  | base64 --decode > /etc/ssl/nginx/nginx-repo.crt
@@ -21,17 +21,17 @@ wget https://nginx.org/keys/nginx_signing.key
 wget https://cs.nginx.com/static/keys/nginx_signing.key && sudo apt-key add nginx_signing.key
 
 apt-key add nginx_signing.key
-sudo apt-get install apt-transport-https lsb-release ca-certificates wget curl jq gettext vim net-tools 
+sudo apt-get install -y apt-transport-https lsb-release ca-certificates wget curl jq gettext vim net-tools 
 printf "deb https://plus-pkgs.nginx.com/ubuntu `lsb_release -cs` nginx-plus\n" | sudo tee /etc/apt/sources.list.d/nginx-plus.list
 
 wget -P /etc/apt/apt.conf.d https://cs.nginx.com/static/files/90nginx
 
 sudo apt-get update
-sudo apt-get install app-protect
-
+sudo apt-get install -y app-protect
+sleep 10
 sudo nginx -v
 
-wget -O /etc/nginx/nginx.conf https://raw.githubusercontent.com/fchmainy/arm-nginx-vmss/main/nginx.conf
+# wget -O /etc/nginx/nginx.conf https://raw.githubusercontent.com/fchmainy/arm-nginx-vmss/main/nginx.conf
 mv nginx.conf /etc/nginx/nginx.conf
 sudo service nginx start
 
@@ -40,25 +40,23 @@ curl -sk -c cookie.txt -X POST --url 'https://'$1'/api/v1/platform/login' --head
 
 # First, let's get the APIKey. it will be useful to onboard the instances onto the controller
 apikey=$(curl -X GET -b cookie.txt -sk -H "Content-Type: application/json" https://$1/api/v1/platform/global | jq .currentStatus.agentSettings.apiKey)
+echo "here is my controller API Key: $apikey"
 
 # ------ Install NGINX Controller Agent
 vmName=$(hostname -f)
 echo $vmName
 CONTROLLER_URL=https://$1
 echo $CONTROLLER_URL
-
-# ------ Install NGINX Controller Agent
-curl -k -sS -L ${CONTROLLER_URL}/install/controller-agent > install.sh
 export API_KEY=$2
 echo $API_KEY
 
-
-
+# ------ Install NGINX Controller Agent
+curl -k -sS -L ${CONTROLLER_URL}/install/controller-agent > install.sh
 
 CONTROLLER_FQDN=$(awk -F '"' '/controller_fqdn=/ { print $2 }' install.sh)
-echo "${1} ${CONTROLLER_FQDN}" > /etc/hosts
+echo "${1} ${CONTROLLER_FQDN}" >> /etc/hosts
 
-sh ./install.sh -l $3 -i $vmName --insecure
+sh ./install.sh -l $4 -i $vmName --insecure
 
 # ------- Register to the Controller
 # Set the following environment variables
@@ -85,18 +83,9 @@ echo "${1} ${CONTROLLER_FQDN}" >> /etc/hosts
 sh ./install.sh -l $4 -i $HOSTNAME --insecure
 
 # ------- Register to the Controller
-# Set the following environment variables
-# export CTRL_IP
-# export CTRL_FQDN=controller.f5demolab.org
-# export CTRL_USERNAME
-# export CTRL_PASSWORD
-# export LOCATION=aks
-
-
-# export CTRL_FQDN=$(echo $ENV_CONTROLLER_URL | awk -F'https://' '{print $2}' | awk -F':8443' '{print $1}')
 
 # Create Environment
-curl --connect-timeout 30 --retry 10 --retry-delay 5 -sk -b cookie.txt -c cookie.txt -X POST -d '{"metadata":{"name":"'$4'"}}' --header 'Content-Type: application/json' --url 'https://'$1'/api/v1/services/environments'
+curl --connect-timeout 30 --retry 10 --retry-delay 5 -sk -b cookie.txt -c cookie.txt -X POST -d '{"metadata":{"name":"'$9'"}}' --header 'Content-Type: application/json' --url 'https://'$1'/api/v1/services/environments'
 
 gwExists=$(curl -sk -b cookie.txt -c cookie.txt  --header 'Content-Type: application/json' --url 'https://'$1'/api/v1/services/environments/'$4'/gateways/'$5 --write-out '%{http_code}' --silent --output /dev/null)
 echo $gwExists
@@ -118,47 +107,18 @@ fi
 curl --connect-timeout 30 --retry 10 --retry-delay 5 -sk -b cookie.txt -c cookie.txt -X PUT -d @gwPayload.json --header 'Content-Type: application/json' --url 'https://'$1'/api/v1/services/environments/'$4'/gateways/'$5
 
 #---------- Remove Agent at VM Destruction -------------
-
-function removeAgent {
-    # Authenticate to controller with credentials in order to get the Session Token
-    curl --connect-timeout 30 --retry 10 --retry-delay 5 -sk -c cookie.txt -X POST --url 'https://'$1'/api/v1/platform/login' --header 'Content-Type: application/json' --data '{"credentials": {"type": "BASIC","username": "'"$6"'","password": "'"$7"'"}}'
-    # 1. Remove the instance from gateway
-    curl --connect-timeout 30 --retry 10 --retry-delay 5 -sk -b cookie.txt -c cookie.txt  --header 'Content-Type: application/json' --url 'https://'$1'/api/v1/services/environments/'$4'/gateways/'$5 -o update.json
-    jq '.desiredState.ingress.placement.instanceRefs -= [{"ref": "/infrastructure/locations/'$4'/instances/'$HOSTNAME'"}]' update.json > $HOSTNAME.json
-    # cat $HOSTNAME.json
-    curl --connect-timeout 30 --retry 10 --retry-delay 5 -sk -b cookie.txt -c cookie.txt -X PUT -d @$HOSTNAME.json --header 'Content-Type: application/json' --url 'https://'$1'/api/v1/services/environments/'$LOCATION'/gateways/'$5
-    # 2. Remove the instance from infrastructure
-    curl --connect-timeout 30 --retry 10 --retry-delay 5 -sk -b cookie.txt -c cookie.txt  --header 'Content-Type: application/json' -X DELETE --url 'https://'$1'/api/v1/infrastructure/locations/'$4'/instances/'$HOSTNAME
-
-}
-
-trap removeAgent 1 2 3 9 15
-
-
-#-----------
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt install -y docker-ce
-
-sudo apt update
-sudo apt install -y conntrack coreutils curl ebtables ethtool gettext grep gzip iproute2 iptables jq less openssl sed socat tar util-linux wget
-
-# $1: parameters('controllerTarBallURL'),
-# $2: parameters('controller_fqdn'), 
-# $3: parameters('adminUsername'), 
-# $4: parameters('adminPassword'),
-# $5: parameters('userEmail'))]"
-# $6: parameters('controllerLicense'))]"
-
-wget $1 -O /home/$3/controller.tar.gz
-cd /home/$3
-tar -xvf controller.tar.gz
-chown -R $3 /home/$3/controller-installer
-
-echo 'now installing controller...'
-/bin/su -c "/home/$3/controller-installer/install.sh -y --self-signed-cert --non-interactive --tsdb-volume-type local -m localhost -x 25 -g false -b false -l $3 -q $4 -j controller@f5demolab.org -f $2 -t admin -u f5demo -e $5 -p $4" - $3
-
-echo 'licensing controller...'
-curl -X POST -sk -H 'Content-Type: application/json' -d '{"credentials": {"type":"BASIC","username":"'$5'","password":"'$4'"}}' https://127.0.0.1/api/v1/platform/login -c cookie.txt
-curl -X PUT -b cookie.txt -sk -H "Content-Type: application/json" -d '{"metadata": {"name":"license"},"desiredState":{"content": "'$6'"}}' https://127.0.0.1/api/v1/platform/license
+#
+#function removeAgent {
+#    # Authenticate to controller with credentials in order to get the Session Token
+#    curl --connect-timeout 30 --retry 10 --retry-delay 5 -sk -c cookie.txt -X POST --url 'https://'$1'/api/v1/platform/login' --header 'Content-Type: application/json' --data '{"credentials": {"type": "BASIC","username": "'"$6"'","password": "'"$7"'"}}'
+#    # 1. Remove the instance from gateway
+#    curl --connect-timeout 30 --retry 10 --retry-delay 5 -sk -b cookie.txt -c cookie.txt  --header 'Content-Type: application/json' --url 'https://'$1'/api/v1/services/environments/'$4'/gateways/'$5 -o update.json
+#    jq '.desiredState.ingress.placement.instanceRefs -= [{"ref": "/infrastructure/locations/'$4'/instances/'$HOSTNAME'"}]' update.json > $HOSTNAME.json
+#    # cat $HOSTNAME.json
+#    curl --connect-timeout 30 --retry 10 --retry-delay 5 -sk -b cookie.txt -c cookie.txt -X PUT -d @$HOSTNAME.json --header 'Content-Type: application/json' --url 'https://'$1'/api/v1/services/environments/'$LOCATION'/gateways/'$5
+#    # 2. Remove the instance from infrastructure
+#    curl --connect-timeout 30 --retry 10 --retry-delay 5 -sk -b cookie.txt -c cookie.txt  --header 'Content-Type: application/json' -X DELETE --url 'https://'$1'/api/v1/infrastructure/locations/'$4'/instances/'$HOSTNAME
+#
+#}
+#
+#trap removeAgent 1 2 3 9 15
